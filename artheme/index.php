6ac8b2f269bcd8e79e5d0d0206ca59242bc3b1b0
arconsultinglabs <36895713+arconsultinglabs@users.noreply.github.com>
2018-03-13 22:50:35 -0500

    Revert "dynamic content tes"
    This reverts commit e5b68604a6010437c7e716274b53e40d6c7e1f3b.

--- artheme/index.php
+++ artheme/index.php
@@ -3,10 +3,10 @@
 <!-- Hero -->
 
 <main id="content" role="main">
-    <h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+    <article >
+        <h1>Hello AMPHTML World!</h1>
+    </article>
 
- <?php the_content(); ?>
 
 
 <?php get_footer(); ?>
