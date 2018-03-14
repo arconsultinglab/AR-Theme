74cc38b51c432de93bbc4efc23a14b4f81656f33
arconsultinglabs <36895713+arconsultinglabs@users.noreply.github.com>
2018-03-13 22:50:28 -0500

    Revert "v2"
    This reverts commit 2c20d2391fb3314603f8ba6a64f973011056c04b.

--- artheme/index.php
+++ artheme/index.php
@@ -3,10 +3,8 @@
 <!-- Hero -->
 
 <main id="content" role="main">
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-<div id="post">
-<?php the_content(); ?>
-</div> <!-- .post -->
+    <h2 class="blog-post-title"><?php the_title(); ?></h2>
+	<p class="blog-post-meta"> <?php the_content(); ?></p>
 
 
 
